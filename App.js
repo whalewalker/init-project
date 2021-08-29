1cee441cff6139670ab7cb25dc3f6943bf20b7f7
whalewalker <mongodbms@gmail.com>
2021-08-30 00:50:14 +0100

    setup first react-native app

---
+++ App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import React, {useState} from 'react';
+import {Button, StyleSheet, Text, View} from 'react-native';
+
+export default function App() {
+  const [outputText, setOutputText] = useState("Open up App.js to start working on your app!");
+  return (
+    <View style={styles.container}>
+      <Text>{outputText}</Text>
+      <Button title="Change text" onPress={() => setOutputText("Text Changed")}/>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
